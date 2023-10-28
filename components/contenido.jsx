7b943dc402a29f76939b48ba27bc1facbc59e5b9
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-10-28 15:18:46 -0500

    Se modularizó el contenido principal.
    dejo hasta aquí el proyecto  hasta revisar con mayor detalle las utilidades de react

--- components/contenido.jsx
+++ components/contenido.jsx
@@ -7,32 +7,22 @@ const Contenido = params => {
           <h1 className='p-title'>{params.title}</h1>
           <div className='row'>
             <div className='contenido'>
-              <h2>Titulo del contenido (variable)</h2>
-              <p>elementos del contenido, descripcion, tal</p>
-              <ul>
-                <li>caracteristicas 1</li>
-                <li>caracteristicas 2</li>
-                <li>caracteristicas 3</li>
-                <li>caracteristicas 4</li>
-                <li>caracteristicas 5</li>
-              </ul>
-              <code>codigo chulito<br/>
-                asd <br/>
-                asdf <br/>
-                asdf <br/>
-                asdf <br/> 
+              <h2 id='topic-title'>Introducción a HTML</h2>
+              <p>En su corazón, HTML es un lenguaje muy sencillo compuesto de elementos, que se pueden aplicar a piezas de texto para darles un significado diferente en un documento (¿Esto es un párrafo? ¿Esto es una lista con viñetas? ¿Esto es parte de una tabla?), estructura un documento en secciones lógicas (¿Tiene una cabecera? ¿Tres columnas de contenido? ¿Un menú de navegación?), e incrusta contenido como imágenes y vídeos en una página. Este módulo introducirá los dos primeros de estos, e introduce conceptos fundamentales y la sintaxis que necesitas para entender HTML.</p>
+              
+            <h2>Guías</h2>
+            <p>Este módulo contiene los siguientes artículos, que te llevarán a través de toda la teoría básica de HTML, y te proporcionarán una amplia oportunidad de probar algunas habilidades.</p>
 
-              </code>
             </div>
             <div className='indice'>
-              <h1>Indice</h1>
+              <h2 id='sub-t-title'>Indice</h2>
               <ul>
-                <li>parte 1</li>
-                <li>parte 2</li>
-                <li>parte 3</li>
-                <li>parte 4</li>
-                <li>parte 5</li>
-                <li>parte 6</li>
+                <li>
+                  <a href="#topic-title">Introducción</a>
+                </li>
+                <li>
+                  <a href="#sub-t-title">Guías</a>
+                </li>
               </ul>
             </div>
           </div>
