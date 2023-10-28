289d0344468b4b9aef8205884bcbf66997f28fa0
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-10-28 00:39:04 -0500

    Inicio de migracion del contenido a un componente react

--- components/contenido.jsx
+++ components/contenido.jsx
@@ -1,9 +1,44 @@
 import '../styles/contenido.css'
 
-export default function Contenido() {
+const Contenido = params => {
   return (
     <>
-    <p1></p1>
+    <div className='principal'>
+          <h1 className='p-title'>{params.title}</h1>
+          <div className='row'>
+            <div className='contenido'>
+              <h2>Titulo del contenido (variable)</h2>
+              <p>elementos del contenido, descripcion, tal</p>
+              <ul>
+                <li>caracteristicas 1</li>
+                <li>caracteristicas 2</li>
+                <li>caracteristicas 3</li>
+                <li>caracteristicas 4</li>
+                <li>caracteristicas 5</li>
+              </ul>
+              <code>codigo chulito<br/>
+                asd <br/>
+                asdf <br/>
+                asdf <br/>
+                asdf <br/> 
+
+              </code>
+            </div>
+            <div className='indice'>
+              <h1>Indice</h1>
+              <ul>
+                <li>parte 1</li>
+                <li>parte 2</li>
+                <li>parte 3</li>
+                <li>parte 4</li>
+                <li>parte 5</li>
+                <li>parte 6</li>
+              </ul>
+            </div>
+          </div>
+        </div>
     </>
   )
 }
+
+export default Contenido
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contenido from '../components/contenido'
 
 function App() {
 
@@ -32,40 +33,9 @@ function App() {
             <li>Python</li>
           </ul>
         </div>
-        <div className='principal'>
-          <h1 className='p-title'>Titulo del contenido</h1>
-          <div className='row'>
-            <div className='contenido'>
-              <h1>Titulo del contenido (variable)</h1>
-              <p>elementos del contenido, descripcion, tal</p>
-              <ul>
-                <li>caracteristicas 1</li>
-                <li>caracteristicas 2</li>
-                <li>caracteristicas 3</li>
-                <li>caracteristicas 4</li>
-                <li>caracteristicas 5</li>
-              </ul>
-              <code>codigo chulito<br/>
-                asd <br/>
-                asdf <br/>
-                asdf <br/>
-                asdf <br/> 
-
-              </code>
-            </div>
-            <div className='indice'>
-              <h1>Indice</h1>
-              <ul>
-                <li>parte 1</li>
-                <li>parte 2</li>
-                <li>parte 3</li>
-                <li>parte 4</li>
-                <li>parte 5</li>
-                <li>parte 6</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Contenido
+          title='Sintaxis HTML'
+        />
       </main>
     </>
   )
